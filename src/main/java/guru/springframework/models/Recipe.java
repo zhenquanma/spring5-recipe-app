f7505921be19ba547b1a44c810881b7a0e9b5ec3
zhenquan <shinnmaa@gmail.com>
2019-04-25 22:09:30 -0400

    Add one-to-one relationship models.

---
+++ src/main/java/guru/springframework/models/Recipe.java
@@ -0,0 +1,108 @@
+package guru.springframework.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String decription;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer serviings;
+    private String source;
+    private String url;
+    private String direction;
+    //todo add
+    //private Difficulty difficulty;
+
+    @Lob
+    private Byte[] images;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServiings() {
+        return serviings;
+    }
+
+    public void setServiings(Integer serviings) {
+        this.serviings = serviings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public Byte[] getImages() {
+        return images;
+    }
+
+    public void setImages(Byte[] images) {
+        this.images = images;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+
+}
