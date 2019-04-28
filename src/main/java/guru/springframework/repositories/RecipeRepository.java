c3307261e3f00748771e0064b75841dbdf23adf9
zhenquan <shinnmaa@gmail.com>
2019-04-27 21:01:03 -0400

    Added repositories and data initialization.

---
+++ src/main/java/guru/springframework/repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.repositories;
+
+import guru.springframework.models.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
