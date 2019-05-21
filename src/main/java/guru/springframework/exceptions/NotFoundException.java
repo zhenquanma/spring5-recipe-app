fe68c5d9d3226cd328aaa4d0475f2933a89c9566
zhenquan <shinnmaa@gmail.com>
2019-05-20 20:43:43 -0400

    Added NotFoundException page, NumberFormat error handling, and page.

--- src/main/java/guru/springframework/exceptions/NotFoundException.java
+++ src/main/java/guru/springframework/exceptions/NotFoundException.java
@@ -21,4 +21,5 @@ public class NotFoundException extends RuntimeException {
     public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
+
 }
